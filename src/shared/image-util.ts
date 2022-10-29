10a9a0e6959e9d0c328fcf461498ce61198c3ef3
Martin PS <16228305+maartin0@users.noreply.github.com>
2022-10-29 16:01:26 +0100

    add template for image util

---
+++ src/shared/image-util.ts
@@ -0,0 +1,9 @@
+import Image from "@entities/Image";
+
+export function phash(image: Image): string {
+    return '';
+}
+
+export function distance(image0: Image, image1: Image): number {
+    return -1;
+}
